5d091fc4afd1c1634adec84d78189630d60d4f40
FDF94 <fdelfedele@gmail.com>
2021-06-06 17:38:35 -0300

    Add Sound class

---
+++ Classes/Sound.py
@@ -0,0 +1,7 @@
+class Sound:
+
+    def __init__(self, sound: str, direction: str,
+                 is_muffled: bool):
+        self.sound = sound
+        self.direction = direction
+        self.is_muffled = is_muffled
